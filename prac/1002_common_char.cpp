33c92ba7b678e8d252c52e41126da323a3063df3
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-06-08 23:00:01 +0530

    croncommit: update 08-06-2024

--- prac/1002_common_char.cpp
+++ prac/1002_common_char.cpp
@@ -6,26 +6,38 @@ Space:
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <list>
-#include <deque>
-#include <stack>
-#include <queue>
-#include <set>
-#include <unordered_set>
-#include <map>
+#include <climits>
 using namespace std;
 
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        sort(words.begin(), words.end());
         vector<string> comms;
-        
+        vector<int> minFreq(26, INT_MAX);
+        int i;
+
+        for(string w: words){
+            vector<int> tempFreq(26, 0);
+            for(char c: w){
+                tempFreq[c-'a']++;
+            }
+            for(i=0;i<26;i++){
+                minFreq[i] = min(minFreq[i], tempFreq[i]);
+            }           
+        }
+        for(i=0;i<26;i++){
+            while(minFreq[i]){
+                comms.push_back(string(1, 'a'+i));
+                minFreq[i]--;
+            }
+        }
+
+        return comms;
     }
 };
 
 int main() {
-    vector<string> s1 = {"bella","label","roller"};
+    vector<string> s1 = {"cool","lock","cook"}; //{"bella","label","roller"};
     Solution s;
     for(auto x: s.commonChars(s1)){
         cout << x << " ";
