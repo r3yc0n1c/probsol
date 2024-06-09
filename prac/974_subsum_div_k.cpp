e7312501f0d7e8f70cbe8b778841ebc6fb4f88d8
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-06-09 23:00:01 +0530

    croncommit: update 09-06-2024

---
+++ prac/974_subsum_div_k.cpp
@@ -0,0 +1,38 @@
+/*
+OJ: https://leetcode.com/problems/subarray-sums-divisible-by-k/
+Time: 
+Space: 
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int> u;
+        int sum = 0, subArrCount = 0, r;
+        u[0] = 1;
+        
+        for(int n: nums){
+            sum += n;
+            r = (n < 0) ? (sum % k + k)%k : (sum%k);
+            
+            if(u[r]) subArrCount += u[r];
+
+            u[r]++;
+        }
+
+        return subArrCount;
+    }
+};
+
+int main() {
+    vector<int> v = {4,5,0,-2,-3,1};
+    int k=5;
+    Solution s;
+    cout << s.subarraysDivByK(v, k)  << endl;
+    return 0;
+}
