8775e74006df8af3e9b6791638351b5013c6ed47
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-07-23 23:00:01 +0530

    croncommit: update 23-07-2024

---
+++ prac/1636_sort_arr_freq.cpp
@@ -0,0 +1,36 @@
+/*
+OJ: https://leetcode.com/problems/sort-array-by-increasing-frequency/description/
+Time: 
+Space: 
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        int freq[200];
+        vector<int> sorted;
+
+        for()
+    }
+};
+
+int main() {
+    vector<int> v = {1,1,2,2,2,3};
+
+    Solution s;
+    for(auto x: s.frequencySort(v))
+        cout << x << " ";
+    cout << endl;
+    return 0;
+}
