a1c81f94b5f5b8c5ec0b957c763d03a4290512d2
r3yc0n1c <raja.majumdar.01@gmail.com>
2024-08-26 23:00:01 +0530

    croncommit: update 26-08-2024

---
+++ prac/110_bal_bin_tree.cpp
@@ -0,0 +1,51 @@
+/*
+OJ: 
+Time: 
+Space: 
+*/
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <deque>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+using namespace std;
+
+/**
+ Definition for a binary tree node.*/
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isBal;
+    bool isBalanced(TreeNode* root) {
+        if(!root) return false;
+        if(!isBal) return false;
+
+        int lst =
+    }
+};
+
+int main() {
+    TreeNode *r = new TreeNode(3);
+    r->left = new TreeNode(9);
+    r->right = new TreeNode(20);
+    r->right->left = new TreeNode(15);
+    r->right->right = new TreeNode(7);
+
+    Solution s;
+    cout << s.maxDepth(r) << endl;
+    return 0;
+}
